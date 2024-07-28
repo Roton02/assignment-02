73d556a8138ef3d529aea6ba7d1494e37f1a60a4
Roton02 <rph645102@gmail.com>
2024-07-28 15:12:45 +0600

    more testing javascript program

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-console.log( typeof ("4" * 6+5)); //29 
-console.log( typeof ("4" + 6*5)); //430 
-console.log( typeof ("4" * 6-5)); //19
+console.log(  ("4" * 6+5)); //29 
+console.log(  ("4" + 6*5)); //430 
+console.log(  ("4" * 6-5)); //19
+console.log(  ("24" / 4 -5)); //1
